267b61049c4a2286d94dab379ffc3777c8a142b7
Axel Joly <joly.axel@outlook.fr>
2020-05-21 21:18:00 +0800

    Update heroku

--- index.js
+++ index.js
@@ -109,4 +109,7 @@ discordClient.on('message', function(message) {
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 discordClient.login(process.env.DISCORD_TOKEN);
 
-http.createServer(function (request, response) {}).listen(process.env.PORT || 5000);
+http.createServer(function (request, response) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(`PvP Stats bot ${process.env.ENV}`);
+}).listen(process.env.PORT || 8080);
